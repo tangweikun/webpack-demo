a4bb3b279cb6a4c1bcd0aaad50d62ad0a7cef758
tangweikun <819573105@qq.com>
2021-02-18 14:53:57 +0800

    清理 /dist 文件夹

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
-  plugins: [new HtmlWebpackPlugin({ title: 'Output Management' })],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ title: 'Output Management' }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
